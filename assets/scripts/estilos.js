d7f8e279bac87e3cbf7ed307d4947b10a72cab4a
thaisregina <ths.mrt.023@gmail.com>
2023-07-07 11:53:43 -0300

    criando folha de estilo js

---
+++ assets/scripts/estilos.js
@@ -0,0 +1,8 @@
+const styles = document.createElement('link');
+styles.setAttribute('rel','stylesheet');
+styles.setAttribute('href','./assets/styles/style.css');
+styles.setAttribute('type','text/css');
+
+    // <link rel = "stylesheet" href = "./assets/style/styles.css">
+
+    document.head.appendChild(styles);

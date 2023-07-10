1371d801252038203ca7c1cb057501868614b7d1
thairegina <ths.mrt.023@gmail.com>
2023-07-10 11:55:10 -0300

    delete com delete pag

--- assets/scripts/estilos.js
+++ assets/scripts/estilos.js
@@ -5,4 +5,21 @@ styles.setAttribute('type','text/css');
 
     // <link rel = "stylesheet" href = "./assets/style/styles.css">
 
-    document.head.appendChild(styles);
+
+const link1 = document.createElement('link');
+link1.rel = "preconnect";
+link1.href = "https://fonts.googleapis.com";
+
+const link2 = document.createElement('link');
+link2.rel = "preconnect";
+link2.href = "https://fonts.gstatic.com";
+link2.crossorigin ="";
+
+const link3 = document.createElement('link');
+link3.rel = "stylesheet";
+link3.href = "https://fonts.googleapis.com/css2?family=Roboto&display=swap";
+
+
+    
+document.head.appendChild(styles);
+document.head.append(ink1,link2,link3)
